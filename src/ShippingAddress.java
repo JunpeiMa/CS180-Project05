b8a3952cbf9d64c13e94745dafa8a80b8db8b019
JunpeiMa <ma571@purdue.edu>
2018-11-29 15:46:42 -0500

    Created constructors, getters, and setters for ShippingAddress class.

--- src/ShippingAddress.java
+++ src/ShippingAddress.java
@@ -2,5 +2,67 @@
  * <h1>Shipping Address</h1> Represents a shipping address
  */
 public class ShippingAddress {
-	//TODO
+
+    public String name;
+    public String address;
+    public String city;
+    public String state;
+    public int zipCode;
+
+    public ShippingAddress () {
+        this.name = "";
+        this.address = "";
+        this.city = "";
+        this.state = "";
+        this.zipCode = 0;
+    }
+
+    public ShippingAddress (String name, String address, String city, String state, int zipCode) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
 }
